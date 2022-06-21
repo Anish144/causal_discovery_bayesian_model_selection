a1243e0e83adf78ba238101340e92c1ffaba0fde
anish144 <anishdhir144@gmail.com>
2022-06-21 10:29:23 +0100

    Checkpoint done some experiments

--- slurm.sh
+++ slurm.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 #SBATCH --gres=gpu:1
 #SBATCH --mail-type=ALL # required to send email notifcations
-#SBATCH --mail-user=<your_username> # required to send email notifcations - please replace <your_username> with your college login name or email address
-export PATH=/vol/bitbucket/ad6013/envs/gp-causal-env/bin/:$PATH
-source activate
+#SBATCH --mail-user=ad6013 # required to send email notifcations - please replace <your_username> with your college login name or email address
 source /vol/cuda/11.2.1-cudnn8.1.0.77/setup.sh
 TERM=vt100 # or TERM=xterm
 /usr/bin/nvidia-smi
-python3 train.py \
+/vol/bitbucket/ad6013/envs/gp-causal-3.8/bin/python3.8 train.py \
     --work_dir="/vol/bitbucket/ad6013/Research/gp-causal" \
     --data="sim" \
-    --num_inducing=100 \
+    --num_inducing=200 \
     --plot_fit \
     --random_restarts=20 \
