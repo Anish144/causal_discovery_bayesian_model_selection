86ff0b7898a6cb9bf3115518fdf210116fe25bab
anish144 <anishdhir144@gmail.com>
2022-07-19 12:44:42 +0100

    HPC runs for gauss pairs added

---
+++ data/gauss_pairs/generate_gauss_pairs.py
@@ -0,0 +1,50 @@
+from concurrent.futures import process
+import os
+from typing import Tuple, Generator
+import numpy as np
+from tqdm import tqdm
+import csv
+
+
+class GaussPairs:
+    def __init__(self, path='./pairs/files'):
+        self.data = dict()
+        file_pairs = f"{path}/CE-Gauss_pairs.csv"
+        with open(file_pairs, 'r') as f:
+            reader = csv.reader(f, delimiter=',')
+            headers = next(reader)
+            data = np.array(list(reader))[:, 1:]
+
+        processed_data = np.zeros((data.shape[0], 1500, 2))
+        for i in range(data.shape[0]):
+            x = np.array(data[i, 0].split()).astype(float)
+            y = np.array(data[i, 1].split()).astype(float)
+            processed_data[i, :, 0],  processed_data[i, :, 1] = x, y
+
+        self.data['cause'] = processed_data[:, :, 0:1]
+        self.data['effect'] = processed_data[:, :, 1:2]
+        self.data['weight'] = np.ones(data.shape[0])
+
+        file_targets = f"{path}/CE-Gauss_targets.csv"
+
+        with open(file_targets, 'r') as f:
+            reader = csv.reader(f, delimiter=',')
+            headers = next(reader)
+            target_data = np.array(list(reader))[:, 1:].astype(float)
+
+        self.data['target'] = target_data
+
+    def return_pairs(self) -> Generator[
+        Tuple[np.ndarray, np.ndarray, float], None, None
+    ]:
+        """
+        Produce a generator object that will yield each of the cause-effect
+        pair datsets.
+
+        Weight factor is also returned to weight the significance of the pair
+        within the whole dataset (used to account for effectively duplicate
+        data across dataset pairs).
+        :return: Tuple of (cause, effect, weight) - cause, effect are 2D numpy
+        arrays
+        """
+        return self.data['cause'], self.data['effect'], self.data['weight'], self.data["target"]
--- phlog_train.sh
+++ phlog_train.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 python3 train.py \
     --work_dir="/vol/bitbucket/ad6013/Research/gp-causal" \
-    --data="cha_pairs" \
+    --data="gauss_pairs" \
     --num_inducing=200 \
     --plot_fit \
-    --random_restarts=20 \
+    --random_restarts=1 \
     --method="gplvm" \
+    --data_start=0 \
+    --data_end=1 \
