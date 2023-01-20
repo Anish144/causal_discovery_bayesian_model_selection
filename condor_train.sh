a3eb4d0eede2e2d3068462d1173147b5efd42225
Anish Dhir <anishdhir144@gmail.com>
2023-01-20 11:11:29 +0000

    Clean up the main code

--- condor_train.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-/vol/bitbucket/ad6013/envs/gp-causal-3.8/bin/python3.8 train.py \
-    --work_dir="/vol/bitbucket/ad6013/Research/gp-causal" \
-    --data="sim" \
-    --num_inducing=500 \
-    --plot_fit \
-    --random_restarts=10 \
