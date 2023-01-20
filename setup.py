2489feae0c4dd5bec139ef428c44b161b98b3838
anish144 <anishdhir144@gmail.com>
2023-01-20 17:48:19 +0000

    New requirements added and removed from setup

--- setup.py
+++ setup.py
@@ -1,13 +1,9 @@
 from setuptools import setup, find_packages
 
 pkgs = find_packages()
-
-req = ["scipy", "gpflow", "tqdm"]
-
 setup(
     name="gplvm_causal_discovery",
     version="0.0.1",
     packages=pkgs,
     long_description=open("README.md").read(),
-    install_requires=req,
 )
