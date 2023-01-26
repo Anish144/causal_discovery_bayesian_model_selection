2681744e503276283ebfc25622c24a87f2cb60ee
Anish Dhir <anishdhir144@gmail.com>
2023-01-26 13:52:23 -0600

    Update run_experiments.py

--- run_experiments.py
+++ run_experiments.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
         "--work_dir",
         "-w",
         type=str,
-        default="/vol/bitbucket/ad6013/Research/gp-causal",
+        default="./",
     )
     parser.add_argument(
         "--num_inducing",
